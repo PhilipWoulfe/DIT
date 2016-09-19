79d568c70d7491b451f9092b2517dd21e313cc21
PhilipWoulfe <philip.woulfe@gmail.com>
2016-09-19 20:04:23 +0100

    Base to and from input sorted. Convert to and from bases sorted

--- Lab Exercise 2.py
+++ Lab Exercise 2.py
@@ -1,26 +1,31 @@
-# converts numbers from decimal to the base supplied
-def convert_number_from_dec(num, base):
+# converts numbers from decimal to the base supplied - returns string
+def convert_number_from_dec(num_int, base):
 
-    if num == 0:
+    num_int = int(num_int)
+
+    if num_int == 0:
         return ""
-    elif num == 1:
-        return str(num)
+    elif num_int == 1:
+        return str(num_int)
     else:
-        result = str(convert_number_from_dec(num // base, base)) + number_to_letter(num % base)
+        result = str(convert_number_from_dec(num_int // base, base)) + number_to_letter(num_int % base)
         return result
 
 
-# converts numbers to decimal from the base supplied
-def convert_number_to_dec(num, base):
+# converts numbers to decimal from the base supplied - returns int
+def convert_number_to_dec(num_str, base):
 
     result = 0
+    counter = len(num_str) - 1
 
-    # todo: fix the rest of this - done?
-    if num == "0":
-        return 0
+    if base == 10:
+        result = int(num_str)
+    elif num_str == "0":
+        result = 0
     else:
-        for x in num:
-            result = (result + letter_to_number(x)) * base
+        for x in num_str:
+            result += letter_to_number(x) * base ** counter
+            counter -= 1
 
     return result
 
@@ -39,34 +44,42 @@ def number_to_letter(num):
 
 # convert a letter to a number (bases between dec and hex)
 def letter_to_number(num):
-    # todo: fix the rest of this - done?
+
     if "0" <= num <= "9":
         return int(num)
     else:
         # convert from decimal to hex etc.
-        return ord(num) - ord("A")
+        return (ord(num) - ord("A")) + 10
     # elif num < 61:
         # return chr(ord('a') + num % 10)
 
 
 # ensure loop runs once
-num_str = "placeholder text"
-base_str = "placeholder text"
+base_from_str = "placeholder text"
+base_to_str = "placeholder text"
 
 # validate inputs
-while not num_str.isdigit():
-    num_str = input("Enter a number to be converted (positive integer): ")
+while not base_from_str.isdigit():
+    base_from_str = input("Enter the base to convert from (base must be between 2 and 16): ")
 
-while not base_str.isdigit():
-    base_str = input("Enter the base to convert to: ")
+while not base_to_str.isdigit():
+    base_to_str = input("Enter the base to convert to (base must be between 2 and 16): ")
 
-num_int = int(num_str)
-base_int = int(base_str)
+# todo validate input based on base from string
+num_str = input("Enter a number to be converted (positive integer): ")
+
+base_from_int = int(base_from_str)
+base_to_int = int(base_to_str)
 
 
-if 0 < base_int <= 16:
-    print(convert_number_from_dec(num_int, base_int))
+if 1 < base_from_int < 17:
+    output = convert_number_to_dec(num_str, base_from_int)
 else:
-    print("Unable to do conversion - base must be a positive integer between 1 and 16!")
+    print("Unable to do conversion - base from must be a positive integer between 1 and 16!")
 
+if 1 < base_to_int < 17:
+    output = str(convert_number_from_dec(int(output), base_to_int))
+else:
+    print("Unable to do conversion - base from must be a positive integer between 1 and 16!")
 
+print(num_str, "in base", base_from_int, "is", output, "in base", base_to_str)
